f0d1a2f26424ca4db9183ef878a03f4d01686e09
Navaneeth <162255937+Navaneeth2707@users.noreply.github.com>
2026-07-14 13:18:53 +0530

    Add demo.autostreamai.in to allowedDevOrigins

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig = {
-  allowedDevOrigins: ['89.116.33.186'],
+  allowedDevOrigins: ['89.116.33.186',
+                     'demo.autostreamai.in',],
 };
 
 export default nextConfig;
